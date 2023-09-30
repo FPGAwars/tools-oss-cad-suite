9929e3244a71ce49d2f04a3ad7e1cd59447b9984
Carlos Venegas Arrabé <6904865+cavearr@users.noreply.github.com>
2023-09-30 23:01:55 +0200

    Update install_darwin.sh
    remove deprecated dependencies

--- scripts/install_darwin.sh
+++ scripts/install_darwin.sh
@@ -10,8 +10,6 @@ install $SOURCE_DIR/libexec/lsusb $PACKAGE_DIR/libexec
 install $SOURCE_DIR/libexec/lsftdi $PACKAGE_DIR/libexec
 install $SOURCE_DIR/libexec/realpath $PACKAGE_DIR/libexec
 
-# -- Copy the ftdi_eeprom file
-install $TOOL_SYSTEM_SRC/bin/ftdi_eeprom $PACKAGE_DIR/bin
 
 # -- Libraries
 install $SOURCE_DIR/lib/libusb-1.0.0.dylib $PACKAGE_DIR/lib
