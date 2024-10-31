fc9b89ee5a56295c0246244f3f2314bea1c4a487
Zapta <zapta@users.noreply.github.com>
2024-10-31 00:07:50 -0700

    Updated the darwin packages.

--- scripts/install_darwin.sh
+++ scripts/install_darwin.sh
@@ -1,13 +1,15 @@
 # This file is sourced in rather than executed a a top level script.
 # Destination directory $PACKAGE_DIR is assumed to be empty.
 
-# -- Copy key directories.
+# -- Copy entire directory trees.
 cp -rp $SOURCE_DIR/bin $PACKAGE_DIR
 cp -rp $SOURCE_DIR/lib $PACKAGE_DIR
 cp -rp $SOURCE_DIR/libexec $PACKAGE_DIR
 cp -rp $SOURCE_DIR/share $PACKAGE_DIR
 cp -rp $SOURCE_DIR/Frameworks $PACKAGE_DIR
+cp -rp $SOURCE_DIR/license $PACKAGE_DIR
 
-# -- Replace bin/nextpnr-ice40 with our simplified template.
-install $TEMPLATE/nextpnr-ice40 $PACKAGE_DIR/bin
-
+# -- Copy individual files.
+install $SOURCE_DIR/activate $PACKAGE_DIR
+install $SOURCE_DIR/environment $PACKAGE_DIR
+install $SOURCE_DIR/README $PACKAGE_DIR
