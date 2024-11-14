97145e1e4528f7162977728a3dd26437b401ef04
Zapta <zapta@users.noreply.github.com>
2024-11-13 17:57:09 -0800

    Changed the tools-oss-cad-suite build scripts to NOT include the ftdi-eeprom binary from the system-tools packges. The system-tools package will be installed as an independent package (sending a parallel PR with that apio change).

--- scripts/install_windows_x64.sh
+++ scripts/install_windows_x64.sh
@@ -8,17 +8,10 @@ assert_dir_empty $PACKAGE_DIR
 rsync -a \
     $SOURCE_DIR/ $PACKAGE_DIR
 
-# -- TODO: Consume this tool from a seperate apio package, to keep the 
-# -- oss-cad-tools package as original as possible.
-# --
-# -- Copy the ftdi_eeprom file from the TOOL_SYSTEM package.
-install $TOOL_SYSTEM_SRC/bin/ftdi_eeprom.exe $PACKAGE_DIR/bin
-
 # -- Sanity checks
 assert_executable $PACKAGE_DIR/bin/yosys.exe
 assert_executable $PACKAGE_DIR/bin/nextpnr-ice40.exe 
 assert_executable $PACKAGE_DIR/bin/nextpnr-ecp5.exe
 assert_executable $PACKAGE_DIR/bin/nextpnr-himbaechel.exe
 assert_executable $PACKAGE_DIR/bin/gtkwave.exe
-assert_executable $PACKAGE_DIR/bin/ftdi_eeprom.exe
 
