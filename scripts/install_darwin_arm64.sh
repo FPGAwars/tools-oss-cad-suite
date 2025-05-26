c3318d27ce0585ed20a298e78d0b9639eb48c6e6
Zapta <zapta@users.noreply.github.com>
2025-05-26 08:53:41 -0700

    Added to mac osx packages the alias libusb-1.0 per https://github.com/FPGAwars/apio/issues/608.

--- scripts/install_darwin_arm64.sh
+++ scripts/install_darwin_arm64.sh
@@ -8,6 +8,9 @@ assert_dir_empty $PACKAGE_DIR
 rsync -a \
     $SOURCE_DIR/ $PACKAGE_DIR
 
+# -- Create an alias, per https://github.com/FPGAwars/apio/issues/608
+cp $PACKAGE_DIR/lib/libusb-1.0.0.dylib cp $PACKAGE_DIR/lib/libusb-1.0.dylib
+
 # -- Sanity checks
 assert_executable $PACKAGE_DIR/bin/yosys
 assert_executable $PACKAGE_DIR/bin/nextpnr-ice40 
@@ -15,3 +18,9 @@ assert_executable $PACKAGE_DIR/bin/nextpnr-ecp5
 assert_executable $PACKAGE_DIR/bin/nextpnr-himbaechel
 assert_executable $PACKAGE_DIR/bin/dot
 assert_executable $PACKAGE_DIR/bin/gtkwave
+
+assert_is_file $PACKAGE_DIR/lib/libusb-1.0.0.dylib
+assert_is_file $PACKAGE_DIR/lib/libusb-1.0.dylib
+
+
+
